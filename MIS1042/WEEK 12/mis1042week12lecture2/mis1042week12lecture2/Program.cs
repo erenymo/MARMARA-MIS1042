ccdef732da4450c9d43e8deb3b65543e2262522f
Eren <erenbalta342115@gmail.com>
2023-05-25 10:25:26 +0300

    week 12 day 2

---
+++ MIS1042/WEEK 12/mis1042week12lecture2/mis1042week12lecture2/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace mis1042week12lecture2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// DATETIME 
+            //int day = 23;
+            //int month = 5;
+            //int year = 2023;
+
+            //int day2 = 25;
+            //int month2 = 5;
+            //int year2 = 2023;
+
+            //int[] date = {2023, 5, 23 };
+            //int[] date2 = {2023, 5, 25};
+            //int[] date3 = { 2023, 26, 5 }; // 0 1 2
+
+            //DateTime myDate = new DateTime(2023,6,15,9,15,30);
+
+            //Console.WriteLine($"Day: {myDate.Day}");
+            //Console.WriteLine($"Month: {myDate.Month}");
+            //Console.WriteLine($"Year: {myDate.Year}");
+            //Console.WriteLine("---");
+            //Console.WriteLine($"Hour: {myDate.Hour}");
+            //Console.WriteLine($"Minute: {myDate.Minute}");
+            //Console.WriteLine($"Second: {myDate.Second}");
+
+            //Console.WriteLine($"Day of the week : {myDate.DayOfWeek}");
+
+            //myDate = myDate.AddDays(20);
+            //myDate = myDate.AddMinutes(20000);
+            //Console.WriteLine("------");
+
+            //Console.WriteLine($"Day: {myDate.Day}");
+            //Console.WriteLine($"Month: {myDate.Month}");
+            //Console.WriteLine($"Year: {myDate.Year}");
+            //Console.WriteLine("---");
+            //Console.WriteLine($"Hour: {myDate.Hour}");
+            //Console.WriteLine($"Minute: {myDate.Minute}");
+            //Console.WriteLine($"Second: {myDate.Second}");
+
+            //Console.WriteLine($"Day of the week : {myDate.DayOfWeek}");
+
+            //Console.Write("Please enter the date : ");
+            //string date = Console.ReadLine();
+
+            //DateTime myDate;
+
+            //bool isConverted = DateTime.TryParse(date, out myDate);
+
+            //if (!isConverted)
+            //{
+            //    Console.WriteLine("Please enter a valid date");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Day: {myDate.Day}");
+            //    Console.WriteLine($"Month: {myDate.Month}");
+            //    Console.WriteLine($"Year: {myDate.Year}");
+            //    Console.WriteLine("---");
+            //    Console.WriteLine($"Hour: {myDate.Hour}");
+            //    Console.WriteLine($"Minute: {myDate.Minute}");
+            //    Console.WriteLine($"Second: {myDate.Second}");
+
+            //    Console.WriteLine($"Day of the week : {myDate.DayOfWeek}");
+            //}
+
+
+        }
+    }
+}
